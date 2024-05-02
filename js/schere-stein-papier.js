e7933e6d0b3b689d22229012e061880d52930170
Anthonykev <kevinanthony9705@gmail.com>
2024-05-02 12:25:22 +0200

    Das Spiel Funktioniert. Draw anzeige hinzugefügt und Draw bugg gelöst

--- js/schere-stein-papier.js
+++ js/schere-stein-papier.js
@@ -1,13 +1,26 @@
+$(document).ready(function () {
+  console.log("JQUERY FUNKTIONIERT!!!");
+  fadeOutIn();
+});
+
 // Hier erstell ich den Array für die Auswahl
 const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
-
 const playerScoreDisplay = document.getElementById("playerScoreDisplay");
 const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+const drawScoreDisplay = document.getElementById("drawScoreDisplay");
 let playerScore = 0;
 let computerScore = 0;
+
+let drawScore = 0;
+function fadeOutIn() {
+  $("#title").animate({ opacity: "0.2" }, 1000, function () {
+    $("#title").animate({ opacity: "1" }, 1000, fadeOutIn);
+  });
+}
+
 function playGame(playerChoice) {
   // ich benutze die Math.random um eine zufälige Zahl zu erzeugen. Ich benutze Math.floor um eine gerade zahl zu kriegen
   const computerChoice = choices[Math.floor(Math.random() * 3)];
@@ -15,7 +28,11 @@ function playGame(playerChoice) {
 
   let result = "";
 
-  if (playerChoice === "rock" && computerChoice === "rock") {
+  if (
+    (playerChoice === "rock" && computerChoice === "rock") ||
+    (playerChoice === "paper" && computerChoice === "paper") ||
+    (playerChoice === "scissors" && computerChoice === "scissors")
+  ) {
     result = "IT'S A TIE!";
   } else {
     switch (playerChoice) {
@@ -36,6 +53,7 @@ function playGame(playerChoice) {
   }
   playerDisplay.textContent = `PLAYER: ${playerChoice}`;
   computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+
   resultDisplay.textContent = result;
 
   resultDisplay.classList.remove("greenText", "redText");
@@ -51,6 +69,10 @@ function playGame(playerChoice) {
       computerScore++;
       computerScoreDisplay.textContent = computerScore;
       break;
+
+    case "IT'S A TIE!":
+      drawScore++;
+      drawScoreDisplay.textContent = drawScore;
 
     default:
       break;
